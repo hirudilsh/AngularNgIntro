6a291135f00d6e2ec58138aa896c156e2ed53a11
Hiruna Dilshan <108188209+hirudilsh@users.noreply.github.com>
2023-05-11 16:31:19 +0530

    @Input Decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'AngularNgIntro';
+
+  parentMessage: string = 'Message coming from Parent component - app.component.ts'
+
+
+
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 export class PostComponent {
 
   text: string = 'List of Posts'
+
+  @Input() fromParent = '';
 
 }
